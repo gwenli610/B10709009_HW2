825394510c203b081e0377af753238a870e7dd39
gwen610 <a950031@gmail.com>
2020-06-17 20:53:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/waitlist/MainActivity.java
@@ -0,0 +1,218 @@
+package com.example.android.waitlist;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.waitlist.data.WaitlistContract;
+import com.example.android.waitlist.data.WaitlistDbHelper;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    private GuestListAdapter mAdapter;
+    private SQLiteDatabase mDb;
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView waitlistRV;
+
+        // Set local attributes to corresponding views
+        waitlistRV = (RecyclerView) this.findViewById(R.id.guests_list_view);
+
+        // Set layout for the RecyclerView, because it's a list we are using the linear layout
+        waitlistRV.setLayoutManager(new LinearLayoutManager(this));
+
+        // Create a DB helper (this will create the DB if run for the first time)
+        WaitlistDbHelper dbHelper = new WaitlistDbHelper(this);
+
+        // Keep a reference to the mDb until paused or killed. Get a writable database
+        // because you will be adding restaurant customers
+        mDb = dbHelper.getWritableDatabase();
+
+        // Get all guest info from the database and save in a cursor
+        Cursor cursor = getAllGuests();
+
+        // Create an adapter for that cursor to display the data
+        mAdapter = new GuestListAdapter(this, cursor);
+
+        // Link the adapter to the RecyclerView
+        waitlistRV.setAdapter(mAdapter);
+
+
+        // Create an item touch helper to handle swiping items off the list
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+
+            // COMPLETED (4) Override onMove and simply return false inside
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                //do nothing, we only care about swiping
+                return false;
+            }
+
+            // COMPLETED (5) Override onSwiped
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                // COMPLETED (8) Inside, get the viewHolder's itemView's tag and store in a long variable id
+                //get the id of the item being swiped
+                final long id = (long) viewHolder.itemView.getTag();
+                // COMPLETED (9) call removeGuest and pass through that id
+                //remove from DB
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("確定要刪除？")
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                removeGuest(id);
+                                mAdapter.swapCursor(getAllGuests());
+                            }
+                        })
+                        .show();
+                mAdapter.swapCursor(getAllGuests());
+            }
+        }).attachToRecyclerView(waitlistRV);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_add:
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                final View v = inflater.inflate(R.layout.jump, null);
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("新增")
+                        .setView(v)
+                        .setNeutralButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText name = (EditText) (v.findViewById(R.id.editTextname));
+                                EditText number = (EditText) (v.findViewById(R.id.editTextnumber));
+                                addToWaitlist(name.getText().toString(),number.getText().toString());
+                            }
+                        })
+                        .show();
+                return true;
+            case R.id.action_setting:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This method is called when user clicks on the Add to waitlist button
+     *
+     *
+     **/
+
+    public void addToWaitlist(String name,String number)
+    {
+        if(name.length()==0||number.length()==0)
+        {
+            return;
+        }
+        int num=1;
+        try{
+            num = Integer.parseInt(number);
+        }catch (NumberFormatException ex)
+        {
+            Log.e(LOG_TAG,"Failed to parse party size text to number"+ex.getMessage());
+        }
+        addNewGuest(name,num);
+        mAdapter.swapCursor(getAllGuests());
+    }
+
+
+    /**
+     * Query the mDb and get all guests from the waitlist table
+     *
+     * @return Cursor containing the list of guests
+     */
+    private Cursor getAllGuests() {
+        return mDb.query(
+                WaitlistContract.WaitlistEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                WaitlistContract.WaitlistEntry.COLUMN_TIMESTAMP
+        );
+    }
+
+    /**
+     * Adds a new guest to the mDb including the party count and the current timestamp
+     *
+     * @param name  Guest's name
+     * @param number Number in party
+     * @return id of new record added
+     */
+    private long addNewGuest(String name, int number) {
+        ContentValues cv = new ContentValues();
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
+        cv.put(WaitlistContract.WaitlistEntry.COLUMN_PARTY_SIZE, number);
+        return mDb.insert(WaitlistContract.WaitlistEntry.TABLE_NAME, null, cv);
+    }
+
+
+    // COMPLETED (1) Create a new function called removeGuest that takes long id as input and returns a boolean
+    /**
+     * Removes the record with the specified id
+     *
+     * @param id the DB id to be removed
+     * @return True: if removed successfully, False: if failed
+     */
+    private boolean removeGuest(long id) {
+        // COMPLETED (2) Inside, call mDb.delete to pass in the TABLE_NAME and the condition that WaitlistEntry._ID equals id
+        return mDb.delete(WaitlistContract.WaitlistEntry.TABLE_NAME, WaitlistContract.WaitlistEntry._ID + "=" + id, null) > 0;
+    }
+
+
+}
